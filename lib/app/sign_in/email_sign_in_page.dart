5922a44bb5841738c359a1cbfe71d1c14f356098
Nuri <nuriddin7612@mail.ru>
2022-05-05 23:27:35 +0900

    time_tracker

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/email_sign_in_bloc_based.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_form_stateful.dart';
 import 'package:time_tracker/services/auth.dart';
 
@@ -12,7 +13,8 @@ class EmailSignInPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Card(child: EmailSignInFormStateful()),
+          child: Card(child: EmailSignInFormBlocBased.create(context),
+          ),
         ),
       ),
       backgroundColor: Colors.grey[200],
