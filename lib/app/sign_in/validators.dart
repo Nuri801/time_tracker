6f8328a319a569667f745606cf25c61b6fb60439
Nuri <nuriddin7612@mail.ru>
2022-05-12 21:28:40 +0900

    time_tracker

--- lib/app/sign_in/validators.dart
+++ lib/app/sign_in/validators.dart
@@ -13,5 +13,5 @@ class EmailAndPasswordValidators {
   final StringValidator emailValidator = NonEmptyStringValidator();
   final StringValidator passwordValidator = NonEmptyStringValidator();
   final String invalidEmailErrorText = 'Email can\'t be free';
-  final String invalidPasswordErrorText = 'Password can\'t be empty';
+  final String invalidPasswordErrorText = 'Password can\'t be free';
 }
