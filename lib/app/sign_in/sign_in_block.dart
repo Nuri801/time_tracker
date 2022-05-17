e2e04fd76fb42ede1118138f21485a92fa0f92da
Nuri <nuriddin7612@mail.ru>
2022-05-17 17:18:31 +0900

    time_tracker

--- lib/app/sign_in/sign_in_block.dart
+++ lib/app/sign_in/sign_in_block.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:time_tracker/services/auth.dart';
 
 class SignInBloc {
-  SignInBloc({required this.auth});
+  SignInBloc({required this.auth, required this.isLoading});
 
   final AuthBase auth;
+  final ValueNotifier<bool> isLoading;
   final StreamController<bool> _isLoadingController = StreamController<bool>();
 
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
